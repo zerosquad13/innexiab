d5ade153edb7fc1fd87cadf9edd02e2c59d63c0e
TeamInnexia <81477304+TeamInnexia@users.noreply.github.com>
2021-08-05 01:41:29 +0530

    Update __main__.py

--- innexiaBot/__main__.py
+++ innexiaBot/__main__.py
@@ -413,7 +413,15 @@ def innexiabot_about_callback(update, context):
                 ]
             ),
         )
-
+    elif query.data == "innexiabot_back":
+        query.message.edit_text(
+                PM_START_TEXT,
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+                disable_web_page_preview=False,
+        )
+            
         
 @run_async
 def Source_about_callback(update, context):

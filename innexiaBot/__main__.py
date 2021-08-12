f141b3a02ea0f8749aff4fc31a87c0879cda6f42
TeamInnexia <81477304+TeamInnexia@users.noreply.github.com>
2021-08-13 00:35:53 +0530

    Update __main__.py

--- innexiaBot/__main__.py
+++ innexiaBot/__main__.py
@@ -87,7 +87,7 @@ PM_START_TEXT = """
 buttons = [
     [
         InlineKeyboardButton(
-            text="➕️ ᴀᴅᴅ ʏᴏɴᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕️", callback_data="aboutmanu_howto"),
+            text="Basic Help", callback_data="aboutmanu_howto")],
     ],
     [
         InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="innexia_"),
